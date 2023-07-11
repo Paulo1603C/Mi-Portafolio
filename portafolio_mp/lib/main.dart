dccb75ec2acd7745b4cb4a6142c717be69146791
Paulo Martinez <paulomartinez1999@gmail.com>
2023-07-11 09:36:25 -0500

    desing 1

--- portafolio_mp/lib/main.dart
+++ portafolio_mp/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(PortafolioApp());
 
@@ -8,10 +8,10 @@ class PortafolioApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Portafolio',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        accentColor: Colors.teal,
         brightness: Brightness.light,
         fontFamily: 'Roboto',
         textTheme: TextTheme(
@@ -29,73 +29,172 @@ class PortafolioPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Color blueMarineColor = Colors.blue;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Mi Portafolio"),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(25),
-            bottomRight: Radius.circular(25),
+            bottomLeft: Radius.circular(5),
+            bottomRight: Radius.circular(5),
           ),
         ),
       ),
-      body: Row(
-        children: [
-          FractionallySizedBox(
-            alignment: Alignment.center,
-            widthFactor: 0.2, // 5 cm de ancho relativo al espacio disponible
-            child: Container(
-              color: Colors.red,
-              // Contenido columna izquierda
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
-                    color: Colors.green,
-                    // Contenido columna central
-                  ),
-                  ListView(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
+      body: Container(
+        color: Color.fromRGBO(37, 36, 64, 1.0),
+        child: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(50.0),
+              child: Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      Container(
-                        color: Colors.blue,
-                        // Contenido columna central con scroll
+                      IconButton(
+                        icon: FaIcon(
+                          FontAwesomeIcons.github,
+                          size: 30,
+                        ),
+                        color: Colors.white,
+                        onPressed: () {
+                          // Acción al hacer clic en el icono de GitHub
+                        },
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      IconButton(
+                        icon: FaIcon(
+                          FontAwesomeIcons.linkedin,
+                          size: 30,
+                        ),
+                        color: Colors.white,
+                        onPressed: () {
+                          // Acción al hacer clic en el icono de LinkedIn
+                        },
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      IconButton(
+                        icon: FaIcon(
+                          FontAwesomeIcons.instagram,
+                          size: 30,
+                        ),
+                        color: Colors.white,
+                        onPressed: () {
+                          // Acción al hacer clic en el icono de Instagram
+                        },
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
             ),
-          ),
-          FractionallySizedBox(
-            alignment: Alignment.center,
-            widthFactor: 0.2, // 5 cm de ancho relativo al espacio disponible
-            child: Container(
-              color: Colors.yellow,
-              // Contenido columna derecha
+            Expanded(
+              flex: 8,
+              child: SingleChildScrollView(
+                  child: Padding(
+                padding: EdgeInsets.all(100.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    ListView(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      children: [
+                        //cada seccion se manera por column -> dentro iran controlados por contendores
+                        //presentacion
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              child: Text(
+                                "Hola, Mi nombre es ",
+                                style: TextStyle(
+                                    color: Colors.white38, fontSize: 20),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 15.0,
+                            ),
+                            Container(
+                              child: AnimatedTextKit(
+                                animatedTexts: [
+                                  TypewriterAnimatedText(
+                                    'Paulo Martínez',
+                                    textStyle: TextStyle(
+                                      fontSize: 50,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    speed: Duration(milliseconds: 200),
+                                  ),
+                                ],
+                                repeatForever: true,
+                              ),
+                            ),
+                            SizedBox(height: 15.0,),
+                            Container(
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 16.0),
+                                child: Text(
+                                    "Soy un estudiante de Ingeniería de Software apasionado por la programación y el desarrollo de software." +
+                                        " Actualmente, estoy buscando una oportunidad para aplicar mis conocimientos y habilidades en un entorno profesional y aprender de los expertos " +
+                                        "en la industria. Estoy interesado en proyectos desafiantes que me permitan crecer y desarrollarme como ingeniero de software.",
+                                    style: TextStyle(
+                                        color: Colors.white70, fontSize: 15)),
+                              ),
+                            ),
+                            SizedBox(height: 15.0),
+                            Container(
+                                child: OutlinedButton(
+                              onPressed: () {
+                                // Acción a realizar cuando se presiona el botón
+                              },
+                              child: Text(
+                                'Presionar',
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.white24,
+                                ),
+                              ),
+                              style: OutlinedButton.styleFrom(
+                                side: BorderSide(
+                                  width: 2,
+                                  color: Colors.white30,
+                                ),
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 50, vertical: 20),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                            ))
+                          ],
+                        ),
+                        //descripcion mia
+                        Row(
+                          children: [
+                            Container(),
+                            Container(),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )),
             ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: Container(
-        height: 80,
-        color: Colors.grey[200],
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '© 2023 Mi Página Web. Todos los derechos reservados.',
-              style: TextStyle(fontSize: 14),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Contacto: info@mipaginaweb.com | Teléfono: 123-456-7890',
-              style: TextStyle(fontSize: 12),
+            Expanded(
+              flex: 1,
+              child: Container(
+                // Contenido columna derecha
+                child: Text("data"),
+              ),
             ),
           ],
         ),
